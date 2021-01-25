15f9dbe4ae9e1099adbb89e60d7e6c61f7bc9292
h1m <himanshu.yadav643@gmail.com>
2021-01-25 07:28:56 +0530

    moved to controlled component with onChange property

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,13 +3,19 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>search anything!</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
