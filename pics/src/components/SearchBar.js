bed86e2596f2744e4bd8f7a563e048bff06490df
h1m <himanshu.yadav643@gmail.com>
2021-01-28 09:11:36 +0530

    refactored the code, solved this issue

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -5,10 +5,15 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>search anything!</label>
             <input
