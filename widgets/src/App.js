cc590070379a5425eff30b617ecd1109d900755e
h1m <himanshu.yadav643@gmail.com>
2021-02-07 12:08:36 +0530

    completed the accordion, understood useState

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 
+const items = [
+  {
+    title: "What is React?",
+    content:
+      "React is the most popular front-end JavaScript library in the field of web development.",
+  },
+  {
+    title: "Why use React?",
+    content:
+      "React allows developers to create large web applications that can change data, without reloading the page. The main purpose of React is to be fast, scalable, and simple. It works only on user interfaces in the application. This corresponds to the view in the MVC template. It can be used with a combination of other JavaScript libraries or frameworks, such as Angular JS in MVC.",
+  },
+];
+
 const App = () => {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items} />
     </div>
   );
 };
--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Accordion = () => {
-  return <h1>Accordion</h1>;
+const Accordion = ({ items }) => {
+  // this is how we use state in functional components
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+    // console.log("Title clicked", index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
+    return (
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
