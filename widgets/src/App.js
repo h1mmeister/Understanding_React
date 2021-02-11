90eaac3d95634af98632e1d65ce9eab4495f02b5
h1m <himanshu.yadav643@gmail.com>
2021-02-11 21:59:55 +0530

    starting Dropdown widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -18,7 +19,7 @@ const items = [
 const App = () => {
   return (
     <div>
-      <Search />
+      <Dropdown />
     </div>
   );
 };
