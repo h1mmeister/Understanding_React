87ab582fc371cf88f9a848a4bae1e7d60a1b8b9a
h1m <himanshu.yadav643@gmail.com>
2021-03-23 01:38:28 +0530

    added action creators and reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,23 @@
+const songsReducer = () => {
+  return [
+    {
+      title: "Tum Hi Ho",
+      duration: "5:00",
+    },
+    {
+      title: "Macarena",
+      duration: "3:15",
+    },
+    {
+      title: "Brown Munde",
+      duration: "4:30",
+    },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
