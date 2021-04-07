be88134d0842f52c1c970cbfdfceb63bab69b117
h1m <himanshu.yadav643@gmail.com>
2021-04-07 13:04:26 +0530

    added userheader for each post

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,5 +2,12 @@ import jsonplaceholder from "../apis/jsonPlaceHolder";
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonplaceholder.get("/posts");
-  dispatch({ type: "FETCH_POSTS", payload: response });
+
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonplaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
