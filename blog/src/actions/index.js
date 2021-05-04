68ef77d09da7dce97fd72154f7936b336514b27e
h1m <himanshu.yadav643@gmail.com>
2021-05-04 15:01:55 +0530

    memoized solution for getting user

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonplaceholder from "../apis/jsonPlaceHolder";
+import _ from "lodash";
 
 // using function to work as an action and then manually using dispatch
 export const fetchPosts = () => async (dispatch) => {
@@ -8,8 +9,12 @@ export const fetchPosts = () => async (dispatch) => {
 };
 
 // same for fetching the author
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonplaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
