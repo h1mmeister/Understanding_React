75831315a803bde61d3f406cf00b36c64178fa05
h1m <himanshu.yadav643@gmail.com>
2021-05-19 01:08:32 +0530

    types around class components, refs

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from "react-dom";
-import GuestList from "./state/GuestList";
+import UserSearch from "./refs/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 const App = () => {
   return (
     <div>
-      <GuestList />
+      <UserSearch />
     </div>
   );
 };
