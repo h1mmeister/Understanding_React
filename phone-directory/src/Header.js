2ec873b39571394bac2e0a288aa3113312d17c85
h1m <himanshu.yadav643@gmail.com>
2021-05-20 11:52:46 +0530

    styling of the phone book app

---
+++ phone-directory/src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  //   const headerStyle = {
+  //     textAlign: "center",
+  //     padding: 20,
+  //     background: "#000",
+  //     color: "#fff",
+  //     textTransform: "uppercase",
+  //   };
+
+  return <div className="header">Phone Directory</div>;
+};
+
+export default Header;
