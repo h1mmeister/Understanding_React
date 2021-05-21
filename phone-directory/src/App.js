5a3acab6d7bfdeaa997706dd3beceb57d686b8de
h1m <himanshu.yadav643@gmail.com>
2021-05-21 13:09:38 +0530

    moving on with phone directory' '

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import "./App.css";
 
 class App extends Component {
   render() {
+    let subscribers = [
+      {
+        id: 1,
+        name: "Himanshu",
+        phone: "8054199262",
+      },
+      {
+        id: 2,
+        name: "Simran",
+        phone: "9815385877",
+      },
+    ];
     return (
       <div>
         <Header />
-        <button>Submit</button>
-        <div>
-          <span>Name</span> <br />
-          <span>Phone</span>
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+          {subscribers.map((sub) => {
+            return (
+              <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
